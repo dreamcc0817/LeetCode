2ea2c07fe1291d7f6b95fd8243da18dbb0e92779
dreamcc <dreamcc0817@gmail.com>
2019-12-18 12:08:13 +0800

    快速排序

--- src/com/dreamcc/arithmetic/QuickSort.java
+++ src/com/dreamcc/arithmetic/QuickSort.java
@@ -10,4 +10,30 @@ package com.dreamcc.arithmetic;
  */
 public class QuickSort {
 
+	private void quickSort(int s[], int l, int r) {
+		if (l < r) {
+			int i = l, j = r, x = s[l];
+			while (i < j) {
+				while (i < j && s[j] >= x) {
+					j--;
+				}
+
+				if (i < j) {
+					s[i++] = s[j];
+				}
+
+				while (i < j && s[i] < x) {
+					i++;
+				}
+
+				if (i < j) {
+					s[j--] = s[i];
+				}
+			}
+
+			s[i] = x;
+			quickSort(s,l,i-1);
+			quickSort(s,i+1,r);
+		}
+	}
 }
